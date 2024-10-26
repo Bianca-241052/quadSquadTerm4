07050a3a17a9816cf0f6cfb76b6442903dd88e86
andrevanheerden <160416636+andrevanheerden@users.noreply.github.com>
2024-10-26 15:17:05 +0200

    save new

---
+++ java/firebase.js
@@ -0,0 +1,92 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+ import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDbmD4rHZbcoIZwSkG03EBaTCCPu3oiQqI",
+   authDomain: "quadsquad-deb87.firebaseapp.com",
+   projectId: "quadsquad-deb87",
+   storageBucket: "quadsquad-deb87.appspot.com",
+   messagingSenderId: "1094931551145",
+   appId: "1:1094931551145:web:679927d51ce598c8dd2ab8"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ function showMessage(message, divId){
+    var messageDiv=document.getElementById(divId);
+    messageDiv.style.display="block";
+    messageDiv.innerHTML=message;
+    messageDiv.style.opacity=1;
+    setTimeout(function(){
+        messageDiv.style.opacity=0;
+    },5000);
+ }
+ //register
+const signUp = document.getElementById('submit-signUp-btn');
+signUp.addEventListener('click', (event) =>{
+    event.preventDefault();
+    const email = document.getElementById('rEmail').value;
+    const password = document.getElementById('registerPassword').value;
+    const username = document.getElementById('rUsername').value;
+
+    const auth=getAuth();
+    const db=getFirestore();
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+        const user=userCredential.user;
+        const userData={
+            email: email,
+            username: username,
+        };
+        showMessage('Account Created Successfully', 'signUpMessage');
+        const docRef=doc(db, "users", user.uid);
+        setDoc(docRef,userData)
+        .then(()=>{
+            window.location.href='../html/home.html'
+        })
+        .catch((error)=>{
+            console.error("error writing document", error);
+    });
+})
+.catch((error)=>{
+    const errorCode=error.code;
+    if(errorCode=='auth/email-already-in-use'){
+        showMessage('Email Address Already Exists !!!', 'signUpMessage');
+    }
+    else{
+        showMessage('unable to create User', 'signUpMessage');
+    }
+})
+});
+
+const signIn=document.getElementById('submit-signIn-btn');
+signIn.addEventListener('click', (event)=>{
+   event.preventDefault();
+   const email=document.getElementById('logEmail').value;
+   const password=document.getElementById('logPassword').value;
+   const auth=getAuth();
+
+   signInWithEmailAndPassword(auth, email,password)
+   .then((userCredential)=>{
+       showMessage('login is successful', 'signInMessage');
+       const user=userCredential.user;
+       localStorage.setItem('loggedInUserId', user.uid);
+       window.location.href='../html/home.html';
+   })
+   .catch((error)=>{
+    const errorCode=error.code;
+    if(errorCode==='auth/invalid-credential'){
+        showMessage('Incorrect Email or Password', 'signInMessage');
+    }
+    else{
+        showMessage('Account does not Exist', 'signInMessage');
+    }
+})
+});;
--- java/script.js
+++ java/script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 const options = {
   method: 'GET',
   headers: {
@@ -116,11 +114,84 @@ fetchPremiereMovies();
 
 
 
-const from = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('username')
-const password = document.getElementById('password')
-const confirmPassword = document.getElementById('confirmPassword')
+//andre
+//signIn page
+const loginBtn = document.querySelector("#login");
+const registerBtn = document.querySelector("#register");
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
 
 
->>>>>>> main
+loginBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "#21264D";
+    registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+
+    loginForm.style.left = "50%";
+    registerForm.style.left = "-50%";
+
+    loginForm.style.opacity = 1;
+    registerForm.style.opacity = 0;
+})
+
+registerBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+    registerBtn.style.backgroundColor = "#21264D";
+
+    loginForm.style.left = "150%";
+    registerForm.style.left = "50%";
+
+    loginForm.style.opacity = 0;
+    registerForm.style.opacity = 1;
+})
+
+const logInputField = document.getElementById('logPassword');
+const logInputIcon = document.getElementById('log-pasword');
+
+const regInputField = document.getElementById('registerPassword');
+const regInputIcon = document.getElementById('reg-pass-icon');
+
+function myLogPassword(){
+    if(logInputField.type === "password"){
+        logInputField.type = "text";
+
+        logInputIcon.name = "eye-off-outline";
+        logInputIcon.style.cursor = "pointer";
+    }
+    else{
+        logInputField.type = "password";
+
+        logInputIcon.name = "eye-outline";
+        logInputIcon.style.cursor = "pointer";
+
+    }
+}
+
+function myRegPassword(){
+    if(regInputField.type === "password"){
+        regInputField.type = "text";
+
+        regInputIcon.name = "eye-off-outline";
+        regInputIcon.style.cursor = "pointer";
+    }
+    else{
+        regInputField.type = "password";
+
+        regInputIcon.name = "eye-outline";
+        regInputIcon.style.cursor = "pointer";
+
+    }
+}
+
+function changeIcon(value){
+    if(value.length > 0){
+        logInputIcon.name = "eye-outline";
+        regInputIcon.name = "eye-outline";
+    }else{
+        logInputIcon.name = "lock-closed-outline";
+        regInputIcon.name = "lock-closed-outline";
+    }
+}
+
+
+
+
