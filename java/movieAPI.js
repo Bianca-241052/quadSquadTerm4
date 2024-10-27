425ccdf314dbce404e656156003b162b903f488d
Bianca-241052 <159829372+Bianca-241052@users.noreply.github.com>
2024-10-27 19:32:31 +0200

    API entered

--- java/movieAPI.js
+++ java/movieAPI.js
@@ -6,7 +6,7 @@ const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer'
+        Authorization:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZDE4OGY5N2I3OTQ3YmIxM2E3N2FhMjgzM2YxZWNiZSIsIm5iZiI6MTcyOTc1OTI5Mi42ODUxNDcsInN1YiI6IjY3MTBjNjFkMWI5MTJhZGQyZWRiY2QwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.79Xo8DabsJftkgYvH8HWG_B108-bF0Km9kTfh2Xycw0'
     }
 };
 
