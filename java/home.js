38ed9dc095cf0d8e59b15911fa582589bf0ab55c
andrevanheerden <160416636+andrevanheerden@users.noreply.github.com>
2024-10-30 20:45:42 +0200

    save

--- java/home.js
+++ java/home.js
@@ -324,11 +324,11 @@ window.addEventListener('DOMContentLoaded', ()=>{
           span.classList.add('fade');
         }, (idx + 1) * 50)
       })
-    }, 2000);
+    }, 2700);
 
     setTimeout(()=>{
-      HomeInIntro.style.top = '-1000vh';
-    }, 2300)
+      HomeInIntro.style.top = '-100vh';
+    }, 2800)
 
   }, 700);//make the start shorter//
 })
